41371a8685de21b95d873790dc5844a33aa153ee
raychz <rchavez9@bu.edu>
2015-05-02 18:21:29 -0400

    Abstracted html -> pho files.

---
+++ html/aboutus.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "utf-8">
+		<title>About Us</title>
+		<link rel="stylesheet" href="../css/styles.css">
+	</head>
+	<body>
+    <?php include("nav.php"); ?>
+
+		<header>
+			<h1 id = "dev">Our web developers</h1>
+		</header>
+		<section>
+		<div id="contentBox" style="margin:0px auto; width:80%">
+
+    		<div id="column1" style="float:left; margin:2%; width:29%;">
+    			<img src="../images/Profile.jpg" class = "centpic" alt="Mr. Casella at charity event" height="400px" width = "250px">
+    			<h2 class = "name"> <i> Alex Casella </i></h2>
+    			<p class = "centp"> Intrigued by revolutionary and ever-changing world of technology, Alex currently studies computer science at Boston University. In addition to a rigorous academic curriculum, Alex is enrolled in the Air Force Reserve Officer Training Corps (ROTC) program where his leadership, character, and discipline is honed. </p>
+    			<p>
+    			<a href="alex.php">To read more about Alex, click here</a>
+				</p>
+    		</div>
+
+    		<div id="column2" style="float:left; margin:2%; width:29%;">
+    			<img src="../images/ray.png" class = "centpic" alt="Raymond Chavez at a restaurant" height="400px" width = "275px">
+     			<h2 class = "name"><i> Raymond Chavez </i></h2>
+     			<p class = "centp"> Born and raised in the beautiful city of El Paso, Texas, Raymond now attends Boston University, where he studies Computer Science and Business Administration at the College of Arts and Sciences. When he's not coding away at the CS Laboratory, you can find him biking along the peaceful Charles River or exploring the vibrant and culturally-rich city of Boston. </p>
+				<p>
+				<a href="raymond.php">To read more about Raymond, click here</a>
+				</p>
+    		</div>
+
+    		<div id="column3" style="float:left; margin:2% ; width:29%">
+    			<img src="../images/max.jpg" class = "centpic" alt="Max Aaronson's favorite picture" height="400px" width="275px">
+     			<h2 class = "name"> <i> Max Aaronson </i></h2>
+				<p class = "centp">Max grew up in Brookline, MA and went to Brookline High School.  He then went to college at 
+				<a href="http://binghamton.edu/index.php">Suny Binghamton University</a> in Binghamton, New York. There he received a bachelors of 
+				science in economic analysis after 4 years.  While at the 
+				university he also got his first experience coding when he took programming classes in C and Python.
+				</p>
+				<p>
+				<a href="max.php">To read more about Max, click here</a>
+				</p>
+    		</div>
+		</div>
+		</section>
+	</body>
+</html>
