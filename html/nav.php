93464f45e65e66aa459b2c7b4e6b2fdee3e1c6c3
raychz <rchavez9@bu.edu>
2015-05-02 17:05:17 -0400

    Abstracted nav file.

---
+++ html/nav.php
@@ -0,0 +1,14 @@
+<nav>
+    <div id="left">
+        <a href=""><img src="../images/karmalogobar2.png" id="logo"></a>
+    </div>
+    <div id="right">
+        <ul>
+            <li><a href="home.php" class="right">Home</a></li>
+            <li><a href="carousel.html" class="right">Screenshots</a></li>
+            <li><a href="jobs.php" class="right">Jobs</a></li>
+            <li><a href="contact.php" class="right">Contact Us</a></li>
+        </ul>
+    </div>
+
+</nav><br>
